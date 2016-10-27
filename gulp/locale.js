1d1cd75a83b63688a9cb2cdfc58a4f3174e9990f
Pirhoo <hello@pirhoo.com>
2016-10-27 15:04:45 +0200

    Added missing keys

--- gulp/locale.js
+++ gulp/locale.js
@@ -1,12 +1,16 @@
 'use strict';
 
+import _    from 'lodash';
 import gulp from 'gulp';
+import jeditor from "gulp-json-editor";
+
 import extractTranslate from 'gulp-angular-translate-extractor';
 import {clientPath} from './paths';
 
 gulp.task('locales', function () {
-  var i18nsrc = [`${clientPath}/{app,components}/**/*.{pug,js,html}`];
+  var i18nsrc  = [`${clientPath}/{app,components}/**/*.{pug,js,html}`];
   var i18ndest = `${clientPath}/assets/locales`;
+  var i18base  = require(`../${clientPath}/assets/locales/en.json`);
   return gulp.src(i18nsrc)
     .pipe(extractTranslate({
       defaultLang: 'en',
@@ -14,6 +18,12 @@ gulp.task('locales', function () {
         dest: i18ndest,
         safeMode: true,
         stringifyOptions: true
+    }))
+    .pipe(jeditor( (json)=> {
+      // Create every missing keys
+      _.keys(i18base).forEach( key=> json[key] = json[key] || '' );
+      // Return the modified json 
+      return json;
     }))
     .pipe(gulp.dest(i18ndest));
 });
