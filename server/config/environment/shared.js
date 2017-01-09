558571f9a0b599b952673d1ebe3f30ac94905c5a
pirhoo <hello@pirhoo.com>
2017-01-09 19:47:22 +0100

    Randomize colors

--- server/config/environment/shared.js
+++ server/config/environment/shared.js
@@ -15,12 +15,13 @@ scheme.from_hue(223)
 
 scheme.colors().map(c=> "#" + c)
 */
+const _ = require('lodash');
 
 exports = module.exports = {
   // List of user roles
   userRoles: ['guest', 'user', 'admin'],
   // Default palette
-  colors: ["#9e0142","#d53e4f","#f46d43","#fdae61","#fee08b","#ffffbf","#e6f598","#abdda4","#66c2a5","#3288bd","#5e4fa2"],
+  colors: _.shuffle(["#9e0142","#d53e4f","#f46d43","#fdae61","#fee08b","#ffffbf","#e6f598","#abdda4","#66c2a5","#3288bd","#5e4fa2"]),
   // Angular Locale
   localeLocation: 'https://code.angularjs.org/1.2.20/i18n/angular-locale_{{locale}}.js'
 };
