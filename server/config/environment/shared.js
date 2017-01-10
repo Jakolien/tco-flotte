2c95af24c16c74f42039ad7ec803531d03ebc926
pirhoo <hello@pirhoo.com>
2017-01-10 11:25:37 +0100

    Updated dep

--- server/config/environment/shared.js
+++ server/config/environment/shared.js
@@ -15,13 +15,12 @@ scheme.from_hue(223)
 
 scheme.colors().map(c=> "#" + c)
 */
-const _ = require('lodash');
 
 exports = module.exports = {
   // List of user roles
   userRoles: ['guest', 'user', 'admin'],
   // Default palette
-  colors: _.shuffle(["#9e0142","#d53e4f","#f46d43","#fdae61","#fee08b","#ffffbf","#e6f598","#abdda4","#66c2a5","#3288bd","#5e4fa2"]),
+  colors: [ "#3288bd", "#ffffbf", "#9e0142", "#abdda4", "#fdae61", "#66c2a5",  "#d53e4f", "#e6f598", "#5e4fa2", "#fee08b", "#f46d43"],
   // Angular Locale
   localeLocation: 'https://code.angularjs.org/1.2.20/i18n/angular-locale_{{locale}}.js'
 };
