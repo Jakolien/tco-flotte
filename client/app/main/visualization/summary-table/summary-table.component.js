200456f4c59f9f417304acce9c78dcbebdde2ac0
pirhoo <hello@pirhoo.com>
2017-02-08 12:35:48 +0100

    Added special group to suummary tables

--- client/app/main/visualization/summary-table/summary-table.component.js
+++ client/app/main/visualization/summary-table/summary-table.component.js
@@ -6,7 +6,14 @@ export default angular.module('oekoFlotteApp.main.summary-table', [])
     template: require('./summary-table.pug'),
     bindings: {
       summaries: '<',
-      fleets: '<'
+      fleets: '<',
+      special: '<'
+    },
+    controller: function() {
+      // Filter group to shoow the special one if needed
+      this.withSpecial = group => {
+        return !group.special || (this.special && group.insights.mileage_special !== 0);
+      };
     }
   })
   .name;
