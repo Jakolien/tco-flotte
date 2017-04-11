36096036b943d13d988ea5f14641980160f8d646
pirhoo <hello@pirhoo.com>
2017-04-11 16:58:58 +0200

    Added reset

--- client/app/app.constants.js
+++ client/app/app.constants.js
@@ -6,6 +6,9 @@ export default angular.module('oekoFlotteApp.constants', [])
   .constant('appConfig', require('../../server/config/environment/shared'))
   .constant('demoScenario', require('../../server/config/demo'))
   .constant('printMode', /PhantomJS/.test(window.navigator.userAgent))
+  .constant('reset', {
+    'car_type': ['fuel_consumption']
+  })
   .constant('valueOrders', {
     energy_type: ['benzin', 'diesel', 'hybrid-benzin', 'hybrid-diesel', 'BEV',
                   'long_distance_train', 'short_distance_train', 'rental_bev',
