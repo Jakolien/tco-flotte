c3a8750893193b7b55c77239cc62ab9f4bfabd3a
Nicolas Kayser-Bril <hi@nkb.fr>
2017-04-20 16:02:02 +0200

    added reset opts

--- client/app/app.constants.js
+++ client/app/app.constants.js
@@ -7,7 +7,21 @@ export default angular.module('oekoFlotteApp.constants', [])
   .constant('demoScenario', require('../../server/config/demo'))
   .constant('printMode', /PhantomJS/.test(window.navigator.userAgent))
   .constant('reset', {
-    'car_type': ['fuel_consumption']
+    'car_type': ["acquisition_price","battery_size","electricity_consumption",
+                 "fixed_costs_car_tax","fixed_costs_check_up","fixed_costs_insurance","fixed_costs_total",
+                 "fuel_consumption","leasing_rate","leasing_residual_value","maintenance_costs_inspection",
+                 "maintenance_costs_repairs","maintenance_costs_tires","maintenance_costs_total","max_battery_charges","reichweite",
+                 "reichweite_NEFZ","residual_value_fixed","residual_value_method","share_electric"],
+    'acquisition_year': ["acquisition_price","battery_size","electricity_consumption",
+                 "fixed_costs_car_tax","fixed_costs_check_up","fixed_costs_insurance","fixed_costs_total",
+                 "fuel_consumption","leasing_rate","leasing_residual_value","maintenance_costs_inspection",
+                 "maintenance_costs_repairs","maintenance_costs_tires","maintenance_costs_total","max_battery_charges","reichweite",
+                 "reichweite_NEFZ","residual_value_fixed","residual_value_method","share_electric"],
+    'car_type': ["acquisition_price","battery_size","electricity_consumption",
+                 "fixed_costs_car_tax","fixed_costs_check_up","fixed_costs_insurance","fixed_costs_total",
+                 "fuel_consumption","leasing_rate","leasing_residual_value","maintenance_costs_inspection",
+                 "maintenance_costs_repairs","maintenance_costs_tires","maintenance_costs_total","max_battery_charges","reichweite",
+                 "reichweite_NEFZ","residual_value_fixed","residual_value_method","share_electric"]
   })
   .constant('valueOrders', {
     energy_type: ['benzin', 'diesel', 'hybrid-benzin', 'hybrid-diesel', 'BEV',
